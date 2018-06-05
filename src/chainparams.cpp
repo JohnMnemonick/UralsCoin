2266802fb00bd84f38659eed014f0bcdb229088c
John Mnemonick <mnemonick.john@gmail.com>
2018-06-05 09:29:02 +0500

    BIP65 activation period fix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -76,7 +76,7 @@ public:
         consensus.nSubsidyHalvingInterval = 210000;
         consensus.BIP34Height = 80000;
         consensus.BIP34Hash = uint256S("0x00000000218e189b4268e72f40e2fab3b95995ab2c4cb195eba1f10ad20c122c");
-        consensus.BIP65Height = 85000; 
+        consensus.BIP65Height = 100000; 
         consensus.BIP66Height = 85000; 
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000");//"00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 6 * 24 * 60 * 60; // two weeks
@@ -94,13 +94,13 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1535760000; // Sep 1 2018 @ 0:00am (UTC)
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1607990400; // 12/15/2020 @ 12:00am (UTC)
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nHeight = 85000;
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nHeight = 100000;
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1535760000; // Sep 1 2018 @ 0:00am (UTC)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1607990400; // 12/15/2020 @ 12:00am (UTC)
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nHeight = 85000;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nHeight = 100000;
 
         // The best chain should have at least this much work.
         //consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000000fe8df6ab2a54f");
