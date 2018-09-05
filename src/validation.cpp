bc719f40807265cb8bf4ca7aeebd4b6ac8dc6768
John Mnemonick <mnemonick.john@gmail.com>
2018-09-05 10:29:17 +0500

    some fixes

--- src/validation.cpp
+++ src/validation.cpp
@@ -2064,6 +2064,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
                           !((pindex->nHeight==91842 && pindex->GetBlockHash() == uint256S("0x00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec")) ||
                            (pindex->nHeight==91880 && pindex->GetBlockHash() == uint256S("0x00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721")));
 
+
     // Once BIP34 activated it was not possible to create new duplicate coinbases and thus other than starting
     // with the 2 existing duplicate coinbase pairs, not possible to create overwriting txs.  But by the
     // time BIP34 activated, in each of the existing pairs the duplicate coinbase had overwritten the first
@@ -3172,9 +3173,10 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
     
     
     if(!IsSporkActive(SPORK_1_MASTERNODE_PAYMENTS_ENFORCEMENT)){
-        MasternodePayments = true; // Uralsdev
+        MasternodePayments = true; 
         if(fDebug) LogPrintf("CheckBlock() : Masternode payment enforcement is off\n");
     }
+
 
     if(MasternodePayments)
     {
@@ -3184,7 +3186,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
         if(pindex != NULL){
             if(pindex->GetBlockHash() == block.hashPrevBlock){
                 CAmount masternodePaymentAmount = GetMasternodePayment(pindex->nHeight+1, block.vtx[0]->GetValueOut());//todo++
-				CAmount hardblockpowreward = block.vtx[0]->vout[0].nValue; //write by Uralsdev 02-06-2015//todo++
+				CAmount hardblockpowreward = block.vtx[0]->vout[0].nValue; 
 				LogPrintf("## Hardblockreward ## CheckBlock() : URALS masternode payments %d\n", hardblockpowreward);
 				bool fIsInitialDownload = IsInitialBlockDownload();
 
